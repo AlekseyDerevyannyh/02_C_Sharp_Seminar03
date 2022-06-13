c03bcea9d3be5f31b1cc090963345a40e05e829c
Aleksey Derevyannyh <DerevyannyhAA@gmail.com>
2022-06-13 23:08:27 +0300

    add HW 1 Palindrom

--- HW/01_Palindrom/Program.cs
+++ HW/01_Palindrom/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 19. Напишите программу, которая принимает на вход пятизначное число
+// и проверяет, является ли оно палиндромом.
+using System;
+using static System.Console;
+
+Clear();
+int number;
+Write ("Введите пятизначное число: ");
+if (!int.TryParse(ReadLine(), out number)) {
+	WriteLine("Ошибка ввода числа!");
+	return;
+}
+if (Math.Abs(number) < 10000 || Math.Abs(number) > 99999) {
+	WriteLine("ОШИБКА: число не пятизначное!");
+	return;
+}
+Write ((number/10000 == number%10) && ((number/1000)%10 == (number/10)%10) ? $"{number} -> да" : $"{number} -> нет");
